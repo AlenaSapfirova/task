620ce45a9a756ccb253243563e27cd7d2c024b99
root <root@LAPTOP-HUMQPPA0.localdomain>
2024-02-03 03:34:02 +0300

    add models, serializers and view

---
+++ task/api/models.py
@@ -0,0 +1,30 @@
+from django.db import models
+# from django.contrib.auth import AbstractUser
+from django.contrib.auth import get_user_model
+from django.core.validators import RegexValidator
+
+User = get_user_model()
+
+
+class Subscribtion(models.Model):
+    author = models.ForeignKey(User, related_name='author', on_delete=models.CASCADE)
+    user = models.ForeignKey(User, related_name='user', on_delete=models.CASCADE)
+
+    class Meta:
+        verbose_name = 'подписка'
+        verbose_name_plural = 'подписки'
+
+
+class Post(models.Model):
+    header = models.CharField(max_length=50)
+    body = models.CharField(max_length=140)
+    date_created = models.DateTimeField(auto_now=True, verbose_name='дата публикации')
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='author_post')
+    is_read_it = models.BooleanField(default=False)
+     
+    def __str__(self):
+        return self.header
+    
+    
+
+
